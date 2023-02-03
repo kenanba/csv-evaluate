c40e309a7c213dbea454791c3f3dfa0637e95d8c
kenan <kenan@prosite.de>
2023-02-03 17:42:17 +0100

    refector: add Validation Plus splite it

--- app/Http/Controllers/CsvEvaluateController.php
+++ app/Http/Controllers/CsvEvaluateController.php
@@ -4,102 +4,138 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ValidateUrls;
 use Carbon\Carbon;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
-use http\Client\Response;
-use Illuminate\Support\Facades\File;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
+/**
+ * @property string $ulid
+ */
 class CsvEvaluateController extends Controller
 {
-    const URL = 'http://127.0.0.1:8000/csv-evaluate?url[]=http://127.0.0.1:8080/api/csv-maker/val1&url[]=http://127.0.0.1:8080/csv-maker/val2';
+    const URL = 'http://127.0.0.1:8000/csv-evaluate?url[]=http://127.0.0.1:8080/api/csv-maker/val1&url[]=http://127.0.0.1:8080/api/csv-maker/val2';
 
-    public function __construct() { }
+    private array $csv = [];
+    private mixed $collectionCsv;
 
-    public function index(ValidateUrls $request)
+    public function __construct()
     {
-        $csv = [];
-        $collectionCsv = [];
-        $ulid = (string)Str::ulid();
-        $mostSpeeches = null;
-        $mostSecurity = '';
-        $leastWordy = '';
+        $this->csv = [];
+        $this->collectionCsv = [];
+        $this->ulid = (string)Str::ulid();
+    }
+
+    public function index(ValidateUrls $request): JsonResponse
+    {
         foreach ($request->query('url') as $key => $url) {
-            $fileName = "$ulid/file_$key.csv";
+            $fileName = "$this->ulid/file_$key.csv";
             $response = Http::withOptions(['timeout' => 60])->get($url);
             if ($response->successful()) {
                 Storage::disk('public')->put($fileName, $response->body());
-                $rows = array_map(
-                    function ($v) {
-                        return str_getcsv($v, ",");
-                    },
+                $rows = array_map(function ($v) { return str_getcsv($v, ","); },
                     file(storage_path('/app/public/').$fileName)
                 );
                 $header = array_shift($rows);
                 foreach ($rows as $row) {
-                    $csv[] = array_combine($header, $row);
+                    $this->csv[] = array_combine($header, $row);
                 }
-                $collectionCsv = collect($csv);
+                $validationResult = $this->validateCsvData($this->csv);
+                if ($validationResult) {
+                    return response()->json($validationResult->original->messages(), 400);
+                }
+                $this->collectionCsv = collect($this->csv);
             } else {
-                return response()->json($url.' => URL is  not valid', 404);
+                return response()->json($url.' => The  URL is  not valid', 404);
             }
         }
-        //2. Which politician gave the most speeches on the topic „Internal Security"?
+        return response()->json([
+            'mostSpeeches' => $this->MostSpeechesPolitician($this->collectionCsv),
+            'mostSecurity' => $this->internalSecurityCheck($this->collectionCsv),
+            'leastWordy' => $this->fewestWords($this->collectionCsv),
+        ], 200);
+    }
+
+    private function validateCsvData(array $csv)
+    {
+        foreach ($csv as $array) {
+            $validatedData = Validator::make($array, [
+                'Speaker' => ['required','string'],
+                'Topic' => ['required','string'],
+                'Date' => ['required','date'],
+                'Words' => ['required','numeric', 'regex:/^[0-9]+$/'],
+            ]);
+            if ($validatedData->fails()) {
+                return response()->json($validatedData->errors(), 400);
+            }
+        }
+    }
+
+    /**
+     * @param $collectionCsv
+     * @return mixed
+     */
+    public function internalSecurityCheck($collectionCsv): mixed
+    {
         $getMostSecurity = $collectionCsv
             ->values()
             ->where('Topic', 'Internal Security')
             ->first();
-        $mostSecurity = $getMostSecurity[ 'Speaker' ];
-        //2. Which politician gave the most speeches on the topic „Internal Security"?
-        //1. Which politician gave the most speeches in 2013?
+        return $getMostSecurity[ 'Speaker' ];
+    }
+
+    /**
+     * @param $collectionCsv
+     * @return string|void
+     */
+    public function MostSpeechesPolitician($collectionCsv)
+    {
         $filtered = $collectionCsv->values()->filter(function ($value, $key) {
-            return Carbon::parse($value[ 'Date' ])->format('Y') >= '2013';
+            return Carbon::parse($value[ 'Date' ])->format('Y') == '2013';
         });
         if (!$filtered->isEmpty()) {
             $result = $filtered->groupBy('Speaker')
                 ->map
                 ->count();
-            $max = $result->max();
-            $maxValues = $result->filter(function ($value) use ($max) {
-                return $value == $max;
-            });
-            if ($maxValues->count() == 1) {
-                $mostSpeeches = $maxValues->keys()->first().' '.$maxValues->values()->first();
-            }
-            if ($maxValues->count() > 1) {
-                foreach ($maxValues as $key => $value) {
-                    $mostSpeeches .= ' '.$key;
-                }
-            }
-            //1. Which politician gave the most speeches in 2013?
+            return $this->checkIfOneOrMoreValues($result, $result->max());
         }
-        //3. Which politician used the fewest words (in total)?
-        $cold = collect();
+    }
+
+    /**
+     * @param $collectionCsv
+     * @return string
+     */
+    public function fewestWords($collectionCsv): string
+    {
         $result = $collectionCsv->values()->groupBy('Speaker')
             ->map(function ($carry, $item) {
                 return $carry->reduce(function ($carry, $item) {
                     return $carry + $item[ 'Words' ];
                 });
             });
-        $min = $result->min();
-        $minValues = $result->filter(function ($value) use ($min) {
-            return $value == $min;
+        return $this->checkIfOneOrMoreValues($result, $result->min());
+    }
+
+    /**
+     * @param $result
+     * @param $itemValue
+     * @return string
+     */
+    public function checkIfOneOrMoreValues($result, $itemValue): string
+    {
+        $response = '';
+        $values = $result->filter(function ($value) use ($itemValue) {
+            return $value == $itemValue;
         });
-        if ($minValues->count() == 1) {
-            $leastWordy = $minValues->keys()->first().' '.$minValues->values()->first();
+        if ($values->count() == 1) {
+            $response = $values->keys()->first().' '.$values->values()->first();
         }
-        if ($minValues->count() > 1) {
-            foreach ($minValues as $key => $value) {
-                $leastWordy .= ' '.$key;
+        if ($values->count() > 1) {
+            foreach ($values as $key => $value) {
+                $response .= ' '.$key;
             }
         }
-        //3. Which politician used the fewest words (in total)?
-        return response()->json([
-            'mostSpeeches' => $mostSpeeches,
-            'mostSecurity' => $mostSecurity,
-            'leastWordy' => $leastWordy,
-        ], 200);
+        return $response;
     }
 }
